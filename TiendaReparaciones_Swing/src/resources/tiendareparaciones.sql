1c86adc621a71d5f831808ff443f06d0bcbd8299
facu <facundoqc56@educastur.es>
2022-05-06 17:56:02 +0200

    sql para la creacion de la base de datos

---
+++ TiendaReparaciones_Swing/src/resources/tiendareparaciones.sql
@@ -0,0 +1,448 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-05-2022 a las 17:54:34
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tiendareparaciones`
+--
+CREATE DATABASE IF NOT EXISTS `tiendareparaciones` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tiendareparaciones`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `baja`
+--
+
+DROP TABLE IF EXISTS `baja`;
+CREATE TABLE `baja` (
+  `idbaja` int(11) NOT NULL,
+  `fechainicio` date NOT NULL,
+  `fechafin` date NOT NULL,
+  `motivobaja` varchar(50) NOT NULL,
+  `idempleado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `nif` varchar(9) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `numerotarjeta` varchar(15) DEFAULT NULL,
+  `cuentacorriente` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+DROP TABLE IF EXISTS `compra`;
+CREATE TABLE `compra` (
+  `idcompra` int(11) NOT NULL,
+  `metododepago` char(2) NOT NULL,
+  `preciototal` decimal(10,0) NOT NULL,
+  `idservicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+CREATE TABLE `empleado` (
+  `idempleado` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `nif` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleadorepartidor`
+--
+
+DROP TABLE IF EXISTS `empleadorepartidor`;
+CREATE TABLE `empleadorepartidor` (
+  `radioreparto` double NOT NULL,
+  `idempleado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleadotaller`
+--
+
+DROP TABLE IF EXISTS `empleadotaller`;
+CREATE TABLE `empleadotaller` (
+  `senior` tinyint(1) DEFAULT NULL,
+  `idempleado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envio`
+--
+
+DROP TABLE IF EXISTS `envio`;
+CREATE TABLE `envio` (
+  `idenvio` int(11) NOT NULL,
+  `fechaenvio` date DEFAULT NULL,
+  `gratuito` tinyint(1) DEFAULT NULL,
+  `destino` varchar(50) NOT NULL,
+  `iservicio` int(11) NOT NULL,
+  `idempleado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+DROP TABLE IF EXISTS `equipo`;
+CREATE TABLE `equipo` (
+  `idequipo` int(11) NOT NULL,
+  `precio` decimal(20,0) DEFAULT NULL,
+  `modelo` varchar(50) DEFAULT NULL,
+  `idcompra` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo`
+--
+
+DROP TABLE IF EXISTS `grupo`;
+CREATE TABLE `grupo` (
+  `nombregrupo` int(11) NOT NULL,
+  `idempleado` int(11) NOT NULL,
+  `idreparacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lote`
+--
+
+DROP TABLE IF EXISTS `lote`;
+CREATE TABLE `lote` (
+  `idlote` int(11) NOT NULL,
+  `descuento` decimal(10,0) DEFAULT NULL,
+  `idequipo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mantenimiento`
+--
+
+DROP TABLE IF EXISTS `mantenimiento`;
+CREATE TABLE `mantenimiento` (
+  `horastrabajadas` int(11) DEFAULT NULL,
+  `idservicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reparacion`
+--
+
+DROP TABLE IF EXISTS `reparacion`;
+CREATE TABLE `reparacion` (
+  `idreparacion` int(11) NOT NULL,
+  `duraciontotal` varchar(10) NOT NULL,
+  `idservicio` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+DROP TABLE IF EXISTS `servicio`;
+CREATE TABLE `servicio` (
+  `idservicio` int(11) NOT NULL,
+  `notas` varchar(100) DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `precioTotal` decimal(10,0) NOT NULL,
+  `idcliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sustituto`
+--
+
+DROP TABLE IF EXISTS `sustituto`;
+CREATE TABLE `sustituto` (
+  `edad` varchar(3) NOT NULL,
+  `colectivo` tinyint(1) NOT NULL,
+  `idempleado` int(11) NOT NULL,
+  `idbaja` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `baja`
+--
+ALTER TABLE `baja`
+  ADD PRIMARY KEY (`idbaja`),
+  ADD KEY `idempleado` (`idempleado`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idcliente`);
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`idcompra`),
+  ADD KEY `foreignkey` (`idservicio`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`idempleado`);
+
+--
+-- Indices de la tabla `empleadorepartidor`
+--
+ALTER TABLE `empleadorepartidor`
+  ADD KEY `foreignkey` (`idempleado`);
+
+--
+-- Indices de la tabla `empleadotaller`
+--
+ALTER TABLE `empleadotaller`
+  ADD KEY `idempleado` (`idempleado`);
+
+--
+-- Indices de la tabla `envio`
+--
+ALTER TABLE `envio`
+  ADD PRIMARY KEY (`idenvio`),
+  ADD KEY `foreignkey` (`iservicio`),
+  ADD KEY `idempleado` (`idempleado`);
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`idequipo`),
+  ADD KEY `idcompra` (`idcompra`);
+
+--
+-- Indices de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD KEY `idempleado` (`idempleado`),
+  ADD KEY `idreparacion` (`idreparacion`);
+
+--
+-- Indices de la tabla `lote`
+--
+ALTER TABLE `lote`
+  ADD PRIMARY KEY (`idlote`),
+  ADD KEY `foreignkey` (`idequipo`);
+
+--
+-- Indices de la tabla `mantenimiento`
+--
+ALTER TABLE `mantenimiento`
+  ADD KEY `foreignkey` (`idservicio`);
+
+--
+-- Indices de la tabla `reparacion`
+--
+ALTER TABLE `reparacion`
+  ADD PRIMARY KEY (`idreparacion`),
+  ADD UNIQUE KEY `foreignkey` (`idservicio`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`idservicio`),
+  ADD KEY `foreignkey` (`idcliente`);
+
+--
+-- Indices de la tabla `sustituto`
+--
+ALTER TABLE `sustituto`
+  ADD KEY `idempleado` (`idempleado`),
+  ADD KEY `idbaja` (`idbaja`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `baja`
+--
+ALTER TABLE `baja`
+  MODIFY `idbaja` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `idcompra` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `idempleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `envio`
+--
+ALTER TABLE `envio`
+  MODIFY `idenvio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  MODIFY `idequipo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `idservicio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `baja`
+--
+ALTER TABLE `baja`
+  ADD CONSTRAINT `baja_ibfk_1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idservicio`) REFERENCES `servicio` (`idservicio`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `empleadorepartidor`
+--
+ALTER TABLE `empleadorepartidor`
+  ADD CONSTRAINT `empleadorepartidor_ibfk_1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `empleadotaller`
+--
+ALTER TABLE `empleadotaller`
+  ADD CONSTRAINT `empleadotaller_ibfk_1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `envio`
+--
+ALTER TABLE `envio`
+  ADD CONSTRAINT `envio_ibfk_1` FOREIGN KEY (`iservicio`) REFERENCES `servicio` (`idservicio`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `envio_ibfk_2` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD CONSTRAINT `equipo_ibfk_1` FOREIGN KEY (`idcompra`) REFERENCES `compra` (`idcompra`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `grupo_ibfk_2` FOREIGN KEY (`idreparacion`) REFERENCES `reparacion` (`idreparacion`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `lote`
+--
+ALTER TABLE `lote`
+  ADD CONSTRAINT `lote_ibfk_1` FOREIGN KEY (`idequipo`) REFERENCES `equipo` (`idequipo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mantenimiento`
+--
+ALTER TABLE `mantenimiento`
+  ADD CONSTRAINT `mantenimiento_ibfk_1` FOREIGN KEY (`idservicio`) REFERENCES `servicio` (`idservicio`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reparacion`
+--
+ALTER TABLE `reparacion`
+  ADD CONSTRAINT `reparacion_ibfk_1` FOREIGN KEY (`idservicio`) REFERENCES `servicio` (`idservicio`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `sustituto`
+--
+ALTER TABLE `sustituto`
+  ADD CONSTRAINT `sustituto_ibfk_1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sustituto_ibfk_2` FOREIGN KEY (`idbaja`) REFERENCES `baja` (`idbaja`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
